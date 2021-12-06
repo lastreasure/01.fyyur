44c941a8bebea9e9cc5830e77ea6c29da3a8a002
Letitia Treasure <letitia.treasure@vodafone.com>
2021-12-06 17:38:31 +0000

    connected to db, modified venue and artist models, implemented migrate

--- starter_code/config.py
+++ starter_code/config.py
@@ -9,5 +9,6 @@ DEBUG = True
 # Connect to the database
 
 
-# TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+# DONE IMPLEMENT DATABASE URL
+SQLALCHEMY_DATABASE_URI = 'postgresql://user1:1234@localhost:5432/fyyurapp'
+SQLALCHEMY_TRACK_MODIFICATIONS = 'false'
